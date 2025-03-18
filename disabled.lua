53e8394bce69837f347f5bf989d71628d1ad2c9f
rayp <151969282+rayparams@users.noreply.github.com>
2025-03-17 20:02:45 -0500

    Create disabled.lua

---
+++ disabled.lua
@@ -0,0 +1,5 @@
+if getgenv().SwiftwareKey == "EREH-YEK-TUPNI" then
+warn("Swiftware is currently DISABLED! This is likely due to an update or a rewrite. Please join the Discord server and check in later.")
+else
+warn("Swiftware key is INVALID, and Swiftware is currently DISABLED! This is likely due to an update or a rewrite. Please join the Discord Server and check in later.")
+end
